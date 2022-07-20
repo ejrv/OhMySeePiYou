5fd3deb94d7fe9ef9c96880734bfb6619ae434a8
Austrakin <67105391+auslie@users.noreply.github.com>
2022-07-20 12:20:26 +0430

    Update STOP DOG.bat

--- STOP DOG.bat
+++ STOP DOG.bat
@@ -1,4 +1,5 @@
 @echo off
 del C:\Users\%username%\AppData\Roaming\Microsoft\Windows\StartMenu\Programs\Startup\OH I WAS SOMETHING JUST LIKE THIS.bat
 del "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\OH I WAS SOMETHING JUST LIKE THIS.bat"
+explorer.exe C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\
 pause
