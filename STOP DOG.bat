b668a37da172581c1e056d3b430319b4cc87574e
Austrakin <67105391+auslie@users.noreply.github.com>
2022-07-20 12:07:04 +0430

    Create STOP DOG.bat

---
+++ STOP DOG.bat
@@ -0,0 +1,4 @@
+@echo off
+del C:\Users\%username%\AppData\Roaming\Microsoft\Windows\StartMenu\Programs\Startup\<filename>   
+del "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\<filename>"
+pause
