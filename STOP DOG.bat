a4c2575a77ac1c89c06409ce40b28acd89b0abef
Austrakin <67105391+auslie@users.noreply.github.com>
2022-07-20 12:08:47 +0430

    Update STOP DOG.bat

--- STOP DOG.bat
+++ STOP DOG.bat
@@ -1,4 +1,4 @@
 @echo off
-del C:\Users\%username%\AppData\Roaming\Microsoft\Windows\StartMenu\Programs\Startup\<filename>   
-del "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\<filename>"
+del C:\Users\%username%\AppData\Roaming\Microsoft\Windows\StartMenu\Programs\Startup\OH I WAS SOMETHING JUST LIKE THIS.bat
+del "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\OH I WAS SOMETHING JUST LIKE THIS.bat"
 pause
